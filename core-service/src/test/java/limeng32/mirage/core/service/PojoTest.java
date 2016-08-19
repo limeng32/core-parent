0e504589d45c7fe7b768316fc564ee8c7a8b8d4e
limeng32 <limeng32@live.cn>
2016-08-19 17:12:28 +0800

    Init project.

---
+++ core-service/src/test/java/limeng32/mirage/core/service/PojoTest.java
@@ -0,0 +1,13 @@
+package limeng32.mirage.core.service;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PojoTest {
+
+	@Test
+	public void firstTest() {
+		Assert.assertTrue(true);
+	}
+
+}
---
+++ core-web/src/test/java/limeng32/mirage/core/web/PojoTest.java
@@ -0,0 +1,13 @@
+package limeng32.mirage.core.web;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PojoTest {
+
+	@Test
+	public void firstTest() {
+		Assert.assertFalse(false);
+	}
+
+}
